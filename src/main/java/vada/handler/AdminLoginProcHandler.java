47e404401ba933acefc73c1d72b7210b4b4ce759
kimmincheols <look753@naver.com>
2022-10-06 21:11:41 +0900

    Upload

---
+++ src/main/java/vada/handler/AdminLoginProcHandler.java
@@ -0,0 +1,55 @@
+package vada.handler;
+
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import vada.dao.impl.LoginDAOImpl;
+import vada.dto.UserDTO;
+import vada.service.LoginService;
+
+public class AdminLoginProcHandler implements CommandHandler {
+
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) {
+
+		String userid = request.getParameter("aduserid");
+		String userpw = request.getParameter("aduserpw");
+
+		LoginService loginService = new LoginDAOImpl();
+
+		List<UserDTO> list=null;
+		PrintWriter script = null;
+		try {
+			list = loginService.adminynLogin(userid, userpw);
+			script = response.getWriter();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		for (UserDTO userDTO : list) {
+			String dbuserid = userDTO.getUserid();
+			String dbuserpw = userDTO.getUserpw();
+			String dbadminyn = userDTO.getAdminyn();
+
+			if (userid.equals(dbuserid) && userpw.equals(dbuserpw)) {
+
+				request.setAttribute("adminyn", dbadminyn);
+				request.setAttribute("userid", userid);
+				System.out.println("adminyn===========>" + dbadminyn);
+
+				return "jsp/adminManageUserForm.jsp";
+			} else {
+				script.println("<script>");
+				script.println("alert('로그인 실패')");
+				script.println("history.back()");
+				script.println("</script>");
+			}
+		}
+
+		return "";
+	}
+
+}
