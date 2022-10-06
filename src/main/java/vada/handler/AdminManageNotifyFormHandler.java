0dbc28b452bb613ae81772c784f7da88e6b35b3c
kimmincheols <look753@naver.com>
2022-10-06 21:15:53 +0900

    Upload

---
+++ src/main/java/vada/handler/AdminManageNotifyFormHandler.java
@@ -0,0 +1,20 @@
+package vada.handler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import vada.dao.impl.BoardListDAOImpl;
+import vada.service.BoardListService;
+
+public class AdminManageNotifyFormHandler implements CommandHandler{
+@Override
+public String process(HttpServletRequest request, HttpServletResponse response) {
+	BoardListService notifyListService = new BoardListDAOImpl();
+	   try {
+		request.setAttribute("list", notifyListService.notifyListBoard());
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	return "/adminmanagenotifyform.do";
+}
+}
