a076640f46a2863d7f455d71ed1acfb0452c9eec
wlgns679 <qkr5778@naver.com>
2022-10-07 17:19:34 +0900

    adminmanage Update

--- src/main/java/vada/handler/AdminManageNotifyFormHandler.java
+++ src/main/java/vada/handler/AdminManageNotifyFormHandler.java
@@ -6,15 +6,16 @@ import javax.servlet.http.HttpServletResponse;
 import vada.dao.impl.BoardListDAOImpl;
 import vada.service.BoardListService;
 
-public class AdminManageNotifyFormHandler implements CommandHandler{
-@Override
-public String process(HttpServletRequest request, HttpServletResponse response) {
-	BoardListService notifyListService = new BoardListDAOImpl();
-	   try {
-		request.setAttribute("list", notifyListService.notifyListBoard());
-	} catch (Exception e) {
-		e.printStackTrace();
+public class AdminManageNotifyFormHandler implements CommandHandler {
+
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) {
+		BoardListService notifyListService = new BoardListDAOImpl();
+		try {
+			request.setAttribute("list", notifyListService.notifyListBoard());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "/jsp/adminManageNotifyForm.jsp";
 	}
-	return "/Vada/adminmanagenotifyform.jsp";
-}
 }
