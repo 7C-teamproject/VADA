5f02c0177464e89e227c02d042ebae6e463ace3c
kimmincheols <look753@naver.com>
2022-10-07 21:33:48 +0900

    Upload

---
+++ src/main/java/vada/handler/NoteMessageListFormHandler.java
@@ -0,0 +1,35 @@
+package vada.handler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import vada.dao.impl.NoteMessageDAOImpl;
+import vada.dto.NoteMessageDTO;
+
+public class NoteMessageListFormHandler implements CommandHandler{
+
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		NoteMessageDAOImpl noteMessageDAOImpl = new NoteMessageDAOImpl();
+		  List<NoteMessageDTO> listmessage = new ArrayList<NoteMessageDTO>();
+		   
+		   NoteMessageDTO noteMessageDTO = new NoteMessageDTO();
+		   noteMessageDTO.setNotetouserid(request.getParameter("notetouserid"));
+		   noteMessageDTO.setNotefromuserid(request.getParameter("notefromid"));
+		   noteMessageDTO.setMessage(request.getParameter("message"));
+		    
+		   
+		   
+		   if(noteMessageDTO!= null) {
+		      listmessage = noteMessageDAOImpl.showboard((String)request.getAttribute("userid"));
+		   }
+		request.setAttribute("listmessage", listmessage);
+		
+		return "jsp/NoteMessageListForm.jsp";
+	}
+	
+}
